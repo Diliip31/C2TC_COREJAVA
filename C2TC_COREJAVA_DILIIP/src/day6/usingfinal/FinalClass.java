359c91d996a24ab621343e2d499aa656bf6dba12
ETHNOTECH <ETHNOTECH@DESKTOP-3JKOMQ0>
2025-07-28 01:05:10 -0700

    day6

---
+++ C2TC_COREJAVA_DILIIP/src/day6/usingfinal/FinalClass.java
@@ -0,0 +1,18 @@
+package day6.usingfinal;
+
+public class FinalClass {
+void show() {
+	System.out.println("Final Class cannot be inheritaed");
+}
+}
+class FinalChildClass extends FinalClass{
+				
+	
+	
+	
+	
+	
+	
+	
+	
+}
